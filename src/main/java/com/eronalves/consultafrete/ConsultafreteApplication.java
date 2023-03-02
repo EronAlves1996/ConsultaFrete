b75c2fd45a123e17e6f264c2b8a6697c5d55b432
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 00:01:34 -0300

    feat: inicializa projeto spring boot

---
+++ src/main/java/com/eronalves/consultafrete/ConsultafreteApplication.java
@@ -0,0 +1,13 @@
+package com.eronalves.consultafrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultafreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultafreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eronalves/consultafrete/ConsultafreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eronalves.consultafrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultafreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
