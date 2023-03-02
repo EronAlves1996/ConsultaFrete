02e49fcf9858b3a0f806e617ef1686cb70d5f088
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 18:55:48 -0300

    refactor: remove get e set do dto

--- src/main/java/com/eronalves/consultafrete/models/dto/ConsultaDto.java
+++ src/main/java/com/eronalves/consultafrete/models/dto/ConsultaDto.java
@@ -4,69 +4,13 @@ import java.util.Objects;
 
 public class ConsultaDto {
 
-	private String cep;
-	private String rua;
-	private String complemento;
-	private String bairro;
-	private String cidade;
-	private String estado;
-	private Double frete;
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getRua() {
-		return rua;
-	}
-
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public Double getFrete() {
-		return frete;
-	}
-
-	public void setFrete(Double frete) {
-		this.frete = frete;
-	}
+	public String cep;
+	public String rua;
+	public String complemento;
+	public String bairro;
+	public String cidade;
+	public String estado;
+	public Double frete;
 
 	@Override
 	public int hashCode() {
