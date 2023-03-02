231e48a7a85f971a51caa51832733e9232c64aaa
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 01:06:43 -0300

    feat: adiciona campos ao dto

--- src/main/java/com/eronalves/consultafrete/models/dto/ConsultaDto.java
+++ src/main/java/com/eronalves/consultafrete/models/dto/ConsultaDto.java
@@ -3,6 +3,12 @@ package com.eronalves.consultafrete.models.dto;
 public class ConsultaDto {
 
 	private String cep;
+	private String rua;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String frete;
 
 	public String getCep() {
 		return cep;
