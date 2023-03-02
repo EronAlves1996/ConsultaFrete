1b03a401f2f48073ea7a108f92abd268514a53b8
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 02:02:52 -0300

    fix: add equal para consertar teste

--- src/main/java/com/eronalves/consultafrete/models/dto/ConsultaDto.java
+++ src/main/java/com/eronalves/consultafrete/models/dto/ConsultaDto.java
@@ -1,5 +1,7 @@
 package com.eronalves.consultafrete.models.dto;
 
+import java.util.Objects;
+
 public class ConsultaDto {
 
 	private String cep;
@@ -64,6 +66,26 @@ public class ConsultaDto {
 
 	public void setFrete(Double frete) {
 		this.frete = frete;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bairro, cep, cidade, complemento, estado, frete, rua);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConsultaDto other = (ConsultaDto) obj;
+		return Objects.equals(bairro, other.bairro) && Objects.equals(cep, other.cep)
+				&& Objects.equals(cidade, other.cidade) && Objects.equals(complemento, other.complemento)
+				&& Objects.equals(estado, other.estado) && Objects.equals(frete, other.frete)
+				&& Objects.equals(rua, other.rua);
 	}
 
 }
--- src/main/java/com/eronalves/consultafrete/models/response/ConsultaResponse.java
+++ src/main/java/com/eronalves/consultafrete/models/response/ConsultaResponse.java
@@ -1,5 +1,7 @@
 package com.eronalves.consultafrete.models.response;
 
+import java.util.Objects;
+
 import org.springframework.beans.BeanUtils;
 
 import com.eronalves.consultafrete.models.dto.ConsultaDto;
@@ -74,6 +76,26 @@ public class ConsultaResponse {
 		ConsultaResponse consultaResponse = new ConsultaResponse();
 		BeanUtils.copyProperties(enderecoDto, consultaResponse);
 		return consultaResponse;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(bairro, cep, cidade, complemento, estado, frete, rua);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConsultaResponse other = (ConsultaResponse) obj;
+		return Objects.equals(bairro, other.bairro) && Objects.equals(cep, other.cep)
+				&& Objects.equals(cidade, other.cidade) && Objects.equals(complemento, other.complemento)
+				&& Objects.equals(estado, other.estado) && Objects.equals(frete, other.frete)
+				&& Objects.equals(rua, other.rua);
 	}
 
 }
