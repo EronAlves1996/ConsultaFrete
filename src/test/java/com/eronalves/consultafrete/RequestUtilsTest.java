2d43bc3d58a5560a629758996d4aecd3a6187539
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 14:00:42 -0300

    feat: extrai a request para implementar tentativas e timeout

--- src/test/java/com/eronalves/consultafrete/RequestUtilsTest.java
+++ src/test/java/com/eronalves/consultafrete/RequestUtilsTest.java
@@ -1,8 +1,14 @@
 package com.eronalves.consultafrete;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestClientException;
 
+import com.eronalves.consultafrete.exception.ApiTimeoutException;
+import com.eronalves.consultafrete.models.response.APIResponse;
 import com.eronalves.consultafrete.utils.RequestUtil;
 
 @SpringBootTest
@@ -10,4 +16,10 @@ public class RequestUtilsTest {
 
 	@Autowired
 	RequestUtil requestUtil;
+
+	@Test
+	public void deveJogarErroEmTimeout() throws RestClientException, InterruptedException {
+		assertThrows(ApiTimeoutException.class,
+				() -> requestUtil.requisitarGet("http://10.255.255.1/", APIResponse.class));
+	}
 }
