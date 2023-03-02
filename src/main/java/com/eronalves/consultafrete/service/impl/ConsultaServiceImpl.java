b2e5a904d27614f475f6431132ee66a881f76e88
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 00:50:27 -0300

    feat: valida cep

--- src/main/java/com/eronalves/consultafrete/service/impl/ConsultaServiceImpl.java
+++ src/main/java/com/eronalves/consultafrete/service/impl/ConsultaServiceImpl.java
@@ -1,15 +1,24 @@
 package com.eronalves.consultafrete.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.eronalves.consultafrete.models.dto.ConsultaDto;
+import com.eronalves.consultafrete.models.dto.Endereco;
 import com.eronalves.consultafrete.service.ConsultaService;
 
 @Service
 public class ConsultaServiceImpl implements ConsultaService {
 
+	@Autowired
+	RestTemplate restTemplate;
+
 	@Override
 	public ConsultaDto consultaFrete(ConsultaDto dto) {
+		if (!Endereco.validaCep(dto.getCep())) {
+
+		}
 	}
 
 }
