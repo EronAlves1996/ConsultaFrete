03bface7a47a7c78a19c24e9d3f85865de93f2a2
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 18:58:28 -0300

    refactor: remove get e set do model response

--- src/main/java/com/eronalves/consultafrete/models/response/ConsultaResponse.java
+++ src/main/java/com/eronalves/consultafrete/models/response/ConsultaResponse.java
@@ -8,69 +8,13 @@ import com.eronalves.consultafrete.models.dto.ConsultaDto;
 
 public class ConsultaResponse {
 
-	private String cep;
-	private String rua;
-	private String complemento;
-	private String bairro;
-	private String cidade;
-	private String estado;
-	private Double frete;
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getRua() {
-		return rua;
-	}
-
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public Double getFrete() {
-		return frete;
-	}
-
-	public void setFrete(Double frete) {
-		this.frete = frete;
-	}
+	public String cep;
+	public String rua;
+	public String complemento;
+	public String bairro;
+	public String cidade;
+	public String estado;
+	public Double frete;
 
 	public static ConsultaResponse from(ConsultaDto enderecoDto) {
 		ConsultaResponse consultaResponse = new ConsultaResponse();
--- src/main/java/com/eronalves/consultafrete/models/response/EnderecoResponse.java
+++ src/main/java/com/eronalves/consultafrete/models/response/EnderecoResponse.java
@@ -5,60 +5,12 @@ import com.eronalves.consultafrete.models.dto.ConsultaDto;
 
 public class EnderecoResponse {
 
-	private String cep;
-	private String logradouro;
-	private String complemento;
-	private String bairro;
-	private String localidade;
-	private String uf;
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getLogradouro() {
-		return logradouro;
-	}
-
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getLocalidade() {
-		return localidade;
-	}
-
-	public void setLocalidade(String localidade) {
-		this.localidade = localidade;
-	}
-
-	public String getUf() {
-		return uf;
-	}
-
-	public void setUf(String uf) {
-		this.uf = uf;
-	}
+	public String cep;
+	public String logradouro;
+	public String complemento;
+	public String bairro;
+	public String localidade;
+	public String uf;
 
 	public void checarNulos() throws CepInexistenteException {
 		if (cep == null && logradouro == null && complemento == null && bairro == null && localidade == null
@@ -68,12 +20,12 @@ public class EnderecoResponse {
 
 	public ConsultaDto toConsultaDto() {
 		ConsultaDto consultaDto = new ConsultaDto();
-		consultaDto.setCep(cep);
-		consultaDto.setBairro(bairro);
-		consultaDto.setCidade(localidade);
-		consultaDto.setComplemento(complemento);
-		consultaDto.setEstado(uf);
-		consultaDto.setRua(logradouro);
+		consultaDto.cep = cep;
+		consultaDto.bairro = bairro;
+		consultaDto.cidade = localidade;
+		consultaDto.complemento = complemento;
+		consultaDto.estado = uf;
+		consultaDto.rua = logradouro;
 		return consultaDto;
 	}
 
