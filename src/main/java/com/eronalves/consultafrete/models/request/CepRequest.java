b5eb4088f43ee6b4ef5533812ddf83932934783b
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 00:30:17 -0300

    feat: configura o contrato do endpoint

--- src/main/java/com/eronalves/consultafrete/models/request/CepRequest.java
+++ src/main/java/com/eronalves/consultafrete/models/request/CepRequest.java
@@ -2,4 +2,14 @@ package com.eronalves.consultafrete.models.request;
 
 public class CepRequest {
 
+	private String cep;
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
 }
--- src/main/java/com/eronalves/consultafrete/models/response/ConsultaResponse.java
+++ src/main/java/com/eronalves/consultafrete/models/response/ConsultaResponse.java
@@ -2,4 +2,68 @@ package com.eronalves.consultafrete.models.response;
 
 public class ConsultaResponse {
 
+	private String cep;
+	private String rua;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String frete;
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getFrete() {
+		return frete;
+	}
+
+	public void setFrete(String frete) {
+		this.frete = frete;
+	}
+
 }
