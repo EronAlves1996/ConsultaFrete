adbbca6a3b5f20be31884aae20d35cdff192f38b
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 17:48:58 -0300

    test: implementa os cenários

--- src/test/java/com/eronalves/consultafrete/testeautomatizado/FormatoCepTest.java
+++ src/test/java/com/eronalves/consultafrete/testeautomatizado/FormatoCepTest.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.eronalves.consultafrete.models.request.CepRequest;
 import com.eronalves.consultafrete.models.response.ConsultaResponse;
+import com.eronalves.consultafrete.models.response.GenericResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -23,7 +26,8 @@ public class FormatoCepTest extends SpringConfig {
 	@Autowired
 	RestTemplate restTemplate;
 
-	ResponseEntity<ConsultaResponse> response;
+	ResponseEntity response;
+	ObjectMapper om = new ObjectMapper();
 
 	@When("o usuario faz uma chamada com CEP {string}")
 	public void o_usuario_faz_uma_chamada_com_cep(String cep) {
@@ -39,10 +43,33 @@ public class FormatoCepTest extends SpringConfig {
 
 	@Then("o usuario recebe informacoes validas \\(nao nulas)")
 	public void o_usuario_recebe_informacoes_validas_nao_nulas() {
-		ConsultaResponse resposta = response.getBody();
+		ConsultaResponse resposta = (ConsultaResponse) response.getBody();
 		System.out.println(resposta);
 		assertNotNull(resposta);
 		assertNotNull(resposta.getBairro());
+		assertNotNull(resposta.getCep());
+		assertNotNull(resposta.getCidade());
+		assertNotNull(resposta.getComplemento());
+		assertNotNull(resposta.getEstado());
+		assertNotNull(resposta.getFrete());
+		assertNotNull(resposta.getRua());
+	}
+
+	@When("o usuario faz uma chamada com CEP invalido {string}")
+	public void o_usuario_faz_uma_chamada_com_cep_invalido(String cep) {
+		CepRequest cepRequest = new CepRequest(cep);
+		try {
+			response = restTemplate.postForEntity("http://localhost:" + port + "/v1/consulta-endereco", cepRequest,
+					ConsultaResponse.class);
+		} catch (HttpClientErrorException ex) {
+			response = ResponseEntity.status(ex.getStatusCode())
+					.body(om.convertValue(ex.getResponseBodyAsString(), GenericResponse.class));
+		}
+	}
+
+	@Then("com a mensagem {string}")
+	public void com_a_mensagem(String string) {
+		GenericResponse body = (GenericResponse) response.getBody();
 	}
 
 }
--- src/test/resources/features/formato_cep_aceito.feature
+++ src/test/resources/features/formato_cep_aceito.feature
@@ -12,21 +12,21 @@ Feature: Formatos aceitos de CEP
 		And o usuario recebe informacoes validas (nao nulas)
 		
 	Scenario: Envia CEP com menos dígitos numéricos
-		When o usuario faz uma chamada com CEP "0100100"
+		When o usuario faz uma chamada com CEP invalido "0100100"
 		Then o usuario recebe um status 400
 		And com a mensagem "Cep não é válido"
 		
 	Scenario: Envia CEP com mais digitos numéricos
-		When o usuario faz uma chamada com CEP "010010010"
+		When o usuario faz uma chamada com CEP invalido "010010010"
 		Then o usuario recebe um status 400
 		And com a mensagem "Cep não é válido"
 		
 	Scenario: Envia CEP com a máscara incorreta (traço na posição incorreta)
-		When o usuario faz uma chamada com CEP "01-001000"
+		When o usuario faz uma chamada com CEP invalido "01-001000"
 		Then o usuario recebe um status 400
 		And com a mensagem "Cep não é válido"
 		
 	Scenario: Envia CEP com algum caractere que não seja numérico
-		When o usuario faz uma chamada com CEP "01001a00"
+		When o usuario faz uma chamada com CEP invalido "01001a00"
 		Then o usuario recebe um status 400
 		And com a mensagem "Cep não é válido"
