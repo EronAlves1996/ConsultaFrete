a216be55588bdb680e9a8cd8fedacace70385903
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 02:31:25 -0300

    feat: add tratamento de erros

---
+++ src/main/java/com/eronalves/consultafrete/models/response/GenericResponse.java
@@ -0,0 +1,11 @@
+package com.eronalves.consultafrete.models.response;
+
+public class GenericResponse {
+
+	private String message;
+
+	public GenericResponse(String message) {
+		this.message = message;
+	}
+
+}
