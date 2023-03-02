3fa04430ef7085887282459f1ff555b3278244d8
Eron Alves <eron.a.silva@capgemini.com>
2023-03-02 01:21:13 -0300

    fix: mapeamento de rota

--- src/main/java/com/eronalves/consultafrete/controllers/ConsultaController.java
+++ src/main/java/com/eronalves/consultafrete/controllers/ConsultaController.java
@@ -1,7 +1,9 @@
 package com.eronalves.consultafrete.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eronalves.consultafrete.exception.ConsultaException;
@@ -10,15 +12,12 @@ import com.eronalves.consultafrete.models.request.CepRequest;
 import com.eronalves.consultafrete.models.response.ConsultaResponse;
 import com.eronalves.consultafrete.service.ConsultaService;
 
-@RestController("v1")
+@RestController
+@RequestMapping("/v1")
 public class ConsultaController {
 
+	@Autowired
 	private ConsultaService consultaService;
-
-	public ConsultaController(ConsultaService consultaService) {
-		super();
-		this.consultaService = consultaService;
-	}
 
 	@PostMapping("/consulta-endereco")
 	public ConsultaResponse consultarFrete(@RequestBody CepRequest cep) throws ConsultaException {
